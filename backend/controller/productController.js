10b39da89397c28f9a33e4dd47bc6d5ecb60ead8
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-12-06 10:57:31 +0530

    Handling Async Error

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,62 +1,56 @@
-const Product = require('../models/productModel')
+const Product = require('../models/productModel');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require('../middleware/catchAsyncErrors')
 
-exports.createProduct = async (req, res, next) => {
+
+exports.createProduct = catchAsyncError(async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
         success: true,
         product
     })
-}
+})
 
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncError(async (req, res) => {
     const products = await Product.find();
 
     res.status(200).json({
         success: true,
         products
     })
-}
+})
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
     let product = await Product.findById(req.params.id);
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message : "Product Not Found"
-        })
+        return next(new ErrorHandler("PRODUCT NOT FOUND", 404))
     }
-    product = await Product.findByIdAndUpdate(req.params.id,req.body,{new : true,runValidators : true,useFindAndModify:false})
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, useFindAndModify: false })
     res.status(200).json({
         success: true,
         product
     })
-}
+})
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message : "PRODUCT NOT FOUND"
-        })
+        return next(new ErrorHandler("PRODUCT NOT FOUND", 404))
     }
     await product.deleteOne();
     res.status(200).json({
         success: true,
-        message : `Product Deleted Successfully with ${req.params.id}`
+        message: `Product Deleted Successfully with ${req.params.id}`
     })
-}
+})
 
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncError(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message: "PRODUCT NOT FOUND"
-        })
+        return next(new ErrorHandler("PRODUCT NOT FOUND", 404))
     }
     res.status(200).json({
         success: true,
         product
     })
-}
+})
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv')
 const connectDatabase = require('./config/database')
+const errorMiddleware = require('./middleware/error')
 app.use(express.json());
 
 dotenv.config({path : "backend/config/config.env"})
@@ -9,7 +10,9 @@ connectDatabase()
 
 
 const product = require('./routes/productRoute')
-app.use('/api/v1',product)
+app.use('/api/v1', product)
+app.use(errorMiddleware);
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
