08d37908b6f3ffebef30ac60978258f51f20884f
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-12-06 15:22:55 +0530

    Api - Login

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require('../middleware/catchAsyncErrors')
 const User = require('../models/userModel');
+const ErrorHandler = require('../utils/errorHandler');
 const errorHandler = require('../utils/errorHandler')
 
 
@@ -14,9 +15,34 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
     })
 
     const token = user.getJWTToken();
-    res.status(200).json({
+    res.status(201).json({
         success: true,
         user,
         token
     })
 });
+
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return next(new ErrorHandler("Please Enter Email & Password", 400))
+    }
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid Email or Password",401))
+    }
+
+    const isPasswordMatched = user.comparePassword(password);
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid Credentials", 401));
+    }
+
+    const token = user.getJWTToken();
+    res.status(200).json({
+        success: true,
+        user,
+        token
+    })
+
+})
