f046db3b1ac7bfb13f73eabfe77fad8027bd2408
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-12-09 11:25:26 +0530

    Add Logout Api

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -37,3 +37,16 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 
 })
 
+exports.logoutUser = catchAsyncError(async (req, res, next) => {
+    // destroy cookie
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly : true
+    })
+
+    res.status(200).json({
+        success: true,
+        message : "Logged Out"
+    })
+})
+
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails} = require("../controller/productController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 
-router.route('/product').get(isAuthenticatedUser, getAllProducts)
-router.route('/product/new').post(createProduct);
-router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getProductDetails)
+router.route('/product').get( getAllProducts)
+router.route('/product/new').post(isAuthenticatedUser, createProduct);
+router.route('/product/:id').put(isAuthenticatedUser, updateProduct).delete(isAuthenticatedUser, deleteProduct).get(getProductDetails)
 module.exports = router;
