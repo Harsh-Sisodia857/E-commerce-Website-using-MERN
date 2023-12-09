872d20a634f4737ead5610f99e5a8a59b5bfc0ae
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-12-09 11:58:53 +0530

    Handle Authorization

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails} = require("../controller/productController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route('/product').get( getAllProducts)
-router.route('/product/new').post(isAuthenticatedUser, createProduct);
-router.route('/product/:id').put(isAuthenticatedUser, updateProduct).delete(isAuthenticatedUser, deleteProduct).get(getProductDetails)
+router.route('/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route('/product/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct).delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct).get(getProductDetails)
 module.exports = router;
