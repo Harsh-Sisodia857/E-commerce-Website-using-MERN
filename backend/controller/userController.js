7ce9001ffe8676b575f8f9cf1cb23be4f13872e2
Harsh-Sisodia857 <92028641+Harsh-Sisodia857@users.noreply.github.com>
2023-12-12 23:15:14 +0530

    Add Cloudinary

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -5,14 +5,22 @@ const ErrorHandler = require('../utils/errorHandler');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require("../utils/sendEmail")
 const crypto = require("crypto")
+const cloudinary = require("cloudinary")
+
 
 exports.registerUser = catchAsyncError(async (req, res, next) => {
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 150,
+        crop : "scale"
+    })
+    
     const { name, email, password } = req.body;
     const user = await User.create({
         name, email, password,
         avatar: {
-            public_id: "this is a sample id",
-            url: "profileUrl"
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url
         }
     })
 
--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,24 @@ const connectDatabase = require('./config/database')
 const errorMiddleware = require('./middleware/error')
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
+const cloudinary = require("cloudinary")
+const bodyParser = require("body-parser")
+const fileUpload = require("express-fileupload")
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended : true}))  
+app.use(fileUpload());
 
 dotenv.config({path : "backend/config/config.env"})
 connectDatabase()
 
-app.use(cookieParser())
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const product = require('./routes/productRoute')
 const user = require("./routes/userRoute")
